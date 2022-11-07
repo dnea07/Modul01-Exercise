69d9c0a872d3dfade58e491cf0371c9c1c238049
dnea07 <nathania07@gmail.com>
2022-11-07 22:01:15 +0700

    day 5 uplod ulang

--- Day05-Exercise-DevinaNathania.js
+++ Day05-Exercise-DevinaNathania.js
@@ -1,6 +1,70 @@
 //Write a function to get the lowest, highest, and average value in the array (with and without sort function)
+let value = [12, 35, 47, 65, 89, 70, 45, 64, 32];
+//cara bukan sort
+let lowHigh = (array) => {
+  //bisa pake cara instan
+  //   let low = Math.min(...array);
+  //   let high = Math.max(...array);
+  //bisa juga pake cara di compare 1 per satu
 
+  let low = array[0];
+  let high = array[0];
+  for (i = 1; i < array.length; i++) {
+    if (array[i] < low) {
+      low = array[i];
+    } else if (array[i] > high) {
+      high = array[i];
+    }
+  }
+
+  let total = 0;
+  for (i = 0; i < array.length; i++) {
+    total += array[i];
+  }
+  let average = total / array.length;
+  let hasil = `GAPAKE SORT\nlowest value = ${low}\nhighest value = ${high}\naverage = ${average}\n`;
+
+  return hasil;
+};
+console.log(lowHigh(value));
+
+// kalau pakai sort
+
+let lowHighSort = (array) => {
+  array.sort();
+  let min = array[0];
+  let max = array[value.length - 1];
+  let total = 0;
+  for (i = 0; i < array.length; i++) {
+    total += array[i];
+  }
+  let avg = total / array.length;
+  let hasil = `PAKE SORT\nmin value = ${min}\nmax value = ${max}\navg = ${avg}\n`;
+  return hasil;
+};
+
+console.log(lowHighSort(value));
 //Write a function that takes an array of words and returns a string by concatenating the words in the array, seperated by commas and - the last word - by an and
+let words = ["kuda", "sushi", "pegawai", "sepeda"];
+
+let combine = (array) => {
+  let hasilAkhir;
+  let result = array[0];
+  for (let i = 1; i < array.length; i++) {
+    if (i < array.length - 1) {
+      let text1 = result;
+      let text2 = array[i];
+      result = text1.concat(", ", text2);
+    } else {
+      let indexTerakhir = array.length - 1;
+      let text3 = array[indexTerakhir];
+      hasilAkhir = result.concat(" and ", text3);
+    }
+  }
+  return hasilAkhir;
+};
+
+console.log(combine(words));
 
 //Write a function to split a string and convert it to array of words (tapi hrs masukin separator)
 let pecah = (words, separator) => {
@@ -73,7 +137,7 @@ let masukFilter = (input) => {
   return element;
 };
 
-console.log(masukFilter("h202"));
+console.log(masukFilter("o2"));
 //write a function to remove all odd numbers in an array and return a new array that contains even numbers only
 numbers = [12, 35, 47, 65, 89, 70, 45, 64, 32];
 let evenOnly = (numberArray) => {
